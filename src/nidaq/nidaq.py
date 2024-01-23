0fdd04e3de98fd60729eec3be2352cb66efe88d8
Alex Striff <abs299@cornell.edu>
2024-01-23 18:31:00 -0500

    daqiv [first execution for new tree]

--- src/nidaq/nidaq.py
+++ src/nidaq/nidaq.py
@@ -565,7 +565,7 @@ def nidaq():
                 "cernoxVB": 1e6,
                 },
             "preamp": {
-                "gain": 100,
+                "gain": 50,
                 "filter": {
                     "mode": "12 dB/oct low-pass",
                     "frequencyHz": 100e3,
