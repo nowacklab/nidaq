3e72adaae01c4d30f9a7db29d6d98922640b632f
Alex Striff <abs299@cornell.edu>
2024-01-23 18:08:45 -0500

    daqiv [first execution for new tree]

--- src/nidaq/nidaq.py
+++ src/nidaq/nidaq.py
@@ -588,7 +588,7 @@ def nidaq():
                     "amplitudeAmps": 150e-6,
                     "stepAmps": 3.0e-9,
                     "regenerations": 1,
-                    "maxFrequency": 10,
+                    "maxFrequency": 1.0,
                     },
                 "input": {
                     "device": deviceName,
