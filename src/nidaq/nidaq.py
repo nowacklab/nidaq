35f639ed0324093b24ad83c666eae98ba6b3ba69
Alex Striff <abs299@cornell.edu>
2024-04-17 19:44:29 -0400

    daqiv [first execution for new tree]

--- src/nidaq/nidaq.py
+++ src/nidaq/nidaq.py
@@ -556,7 +556,7 @@ def nidaq():
             "execution": execution,
             "comment": inspect.cleandoc(f"""
             Others open
-            Source 16, sink 15, V across 14 and 13
+            Source 16, sink 14, V across 3 and 1
             """),
             "cooldown": 3,
             "device": {
