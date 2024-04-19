e8dde2afbd1187bdf6399ed212f6223e52932b42
Alex Striff <abs299@cornell.edu>
2024-04-19 12:09:10 -0400

    daqiv [first execution for new tree]

--- src/nidaq/nidaq.py
+++ src/nidaq/nidaq.py
@@ -556,7 +556,7 @@ def nidaq():
             "execution": execution,
             "comment": inspect.cleandoc(f"""
             Others grounded
-            Source 15, sink 14, V across 13 and 16
+            Source 15, sink 14, V across 16 and 13
             Nulled preamp offset for 10x gain
             """),
             "cooldown": 3,
