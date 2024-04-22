1da99791f39551ce968043f129b849b86a2815a9
Alex Striff <abs299@cornell.edu>
2024-04-22 12:44:16 -0400

    daqiv [first execution for new tree]

--- src/nidaq/nidaq.py
+++ src/nidaq/nidaq.py
@@ -612,8 +612,8 @@ def nidaq():
                 "input": {
                     "device": deviceName,
                     "channel": "ai16",
-                    "minVoltage": -10.0,
-                    "maxVoltage": 10.0,
+                    "minVoltage": -2.0,
+                    "maxVoltage": 2.0,
                     },
                 "daqioDataPath": Path(os.path.relpath(daqioDataPath, parametersPath.parent)).as_posix(),
                 "daqVersionInformation": {
